8be156d9a564faa6ff8caac012ace32f7f295505
Imran Kheraj <ikheraj17@gmail.com>
2020-02-06 13:58:12 -0600

    Added functionality for Diner's Club, Visa, Amex, and Mastercard

--- detectNetwork.js
+++ detectNetwork.js
@@ -9,10 +9,34 @@
 
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
+  var length = cardNumber.length;
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+  var twoNums = '';
+  for (var i = 0; i < 2; i ++){
+   twoNums = twoNums + cardNumber[i];
+  }
+  if((twoNums === "38" || twoNums === "39") && (cardNumber.length === 14)){
+    return "Diner's Club";
+  }
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  if((twoNums === "34" || twoNums === "37") && (cardNumber.length === 15)){
+    return "American Express";
+  }
+  // ---------------------------- VISA --------------------------------------------------------------------------------
+  
+  if((cardNumber[0] === "4") && (length === 13 || length === 16 || length === 19)){
+    return "Visa";
+  }
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  //--------------------------------- MasterCard ------------------------------------------------------------------------
+
+  if((twoNums === "51" || twoNums === "52" || twoNums === "53"|| twoNums === "54"|| twoNums === "55") && (length === 16)){
+    return "MasterCard";
+  }
+
 };
+
+
+
 
 
